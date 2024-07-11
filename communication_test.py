320f8d9020211d34a679f6f0b8a18f34f9364181
yj93.cho <bigeye9312@gmail.com>
2024-07-11 11:16:41 +0900

    [feature] Step 8~9

--- communication_test.py
+++ communication_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from communication import SmsSender
+from communication import SmsSender, MailSender
 
 
 class TestableSmsSender(SmsSender):
@@ -13,6 +13,19 @@ class TestableSmsSender(SmsSender):
 
     def is_send_method_is_called(self):
         return self.__send_method_is_called
+
+
+class TestableMailSender(MailSender):
+    def __init__(self):
+        super().__init__()
+        self.__count_send_mail_is_called = 0
+
+    def send_mail(self, schedule):
+        print("[Test] send SMS")
+        self.__count_send_mail_is_called += 1
+
+    def get_count_send_mail_is_called(self):
+        return self.__count_send_mail_is_called
 
 
 class TestSmsSender(TestCase):
